92467d1e7f7859ff9e8d5e8dce7243dd2046b054
erikyoung <erik.young@rmit.edu.vn>
2018-01-23 12:58:54 +0700

    trying to add teacher viewing of courses and adding sections

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 	resources :courses, only: [:index, :show]
 	namespace :instructor do
 		resources :courses, only: [:new, :create, :show, :index]
+			resources :sections, only: [:new, :create]
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

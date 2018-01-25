b59595991cea3723efa2b82f6adb287c7ca7be53
erikyoung <erik.young@rmit.edu.vn>
2018-01-25 11:54:14 +0700

    set up about and blog access

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 	root 'static_pages#index'
+	get 'about' => "static_pages#about", :as => "about"
+	get 'blog' => "static_pages#blog", :as => "blog"
+
+	#get links for the about page
+	#get link_to for the blog
+	#get link_to for the 
+
+
 	resources :courses, only: [:index, :show]
 	resources :lessons, only: [:show]
 	namespace :instructor do
